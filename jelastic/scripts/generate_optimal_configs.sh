1f036e43858adf49a5b324c25ac25a99ef9d9448
Dmitry Zubelevich <dz@jelastic.com>
2016-06-18 17:45:34 +0300

    JE-26997 [Enable MaxPermSize in container if RAM is > 800MB ( for Java 6, 7)] - adjustment for cartridges

--- jelastic/scripts/generate_optimal_configs.sh
+++ jelastic/scripts/generate_optimal_configs.sh
@@ -8,11 +8,8 @@ NEO4J_WRAPPER_PROP_FILE="${OPENSHIFT_NEO4J_DIR}/versions/${Version}/neo4j-${Vers
 
 [ -e ${OPENSHIFT_NEO4J_DIR}/versions/${Version}/neo4j-${Version}/bin/variablesparser.sh ] && . ${OPENSHIFT_NEO4J_DIR}/versions/${Version}/neo4j-${Version}/bin/variablesparser.sh;
 
-[ -z "$XMS" ] && XMS=32 || XMS=$(echo $XMS | grep -o "[0-9]*")
-[ -z "$XMX" ] && { memory_total=`free -m | grep Mem | awk '{print $2}'`; let XMX=memory_total-35; XMX="${XMX}"; } || XMX=$(echo $XMX | grep -o "[0-9]*")
-
 $SED -i "s/^#dbms.memory.heap.initial_size/dbms.memory.heap.initial_size/g" $NEO4J_WRAPPER_PROP_FILE;
-$SED -i "s/^dbms.memory.heap.initial_size=.*/dbms.memory.heap.initial_size=${XMS}/" $NEO4J_WRAPPER_PROP_FILE;
+$SED -i "s/^dbms.memory.heap.initial_size=.*/dbms.memory.heap.initial_size=32/" $NEO4J_WRAPPER_PROP_FILE;
 
 $SED -i "s/^#dbms.memory.heap.max_size/dbms.memory.heap.max_size/g" $NEO4J_WRAPPER_PROP_FILE;
-$SED -i "s/^dbms.memory.heap.max_size=.*/dbms.memory.heap.max_size=${XMX}/" $NEO4J_WRAPPER_PROP_FILE;
+$SED -i "s/^dbms.memory.heap.max_size=.*/dbms.memory.heap.max_size=${XMX_VALUE}/" $NEO4J_WRAPPER_PROP_FILE;
